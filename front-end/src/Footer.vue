c6fcc479b75ca59ef9432fa39605da2c942dba76
yujin100b <jjem22@naver.com>
2018-08-06 18:37:21 +0900

    header footer vue tutorial ~~

---
+++ front-end/src/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <footer>{{ footerText }}</footer>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            footerText: "This is the footer",
+        }
+    }
+}
+</script>
+<style lang="css">
+</style>
---
+++ front-end/src/Header.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <header>
+        <p>{{ headerText }}</p>
+    </header>
+</template>
+<script>
+
+export default {
+    data() {
+        return {
+            headerText: "This is the header",
+        }
+    }
+}
+
+</script>
+<style >
+</style>
