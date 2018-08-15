4fbe2f1b72063ec18afe5d66838b0166376dae39
yujin100b <jjem22@naver.com>
2018-08-15 15:49:32 +0900

    흑

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,16 +1,22 @@
-/*import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
+export default new VueRouter({
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
     }
+
   ]
 })
-*/
