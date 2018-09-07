cf341b13ec8102e8349acf1eb81cc3b19ee22eb8
ifmoon <ehdals1992@naver.com>
2018-09-07 16:33:43 +0900

    .

--- _frontend/src/components/Mod.vue
+++ _frontend/src/components/Mod.vue
@@ -10,6 +10,8 @@
     <p>Value: {{ text2 }}</p>
     <b-form-file v-model="files" :state="Boolean(files)" multiple placeholder="Choose a file..."></b-form-file>
     <b-button variant="warning" @click="test">실험중</b-button>
+    <b-button variant="danger" @click="test2">실험중2</b-button>
+    <b-button variant="success" @click="test3">실험중3</b-button>
   </div>
 </template>
 <script>
@@ -34,6 +36,21 @@ export default {
       this.$http.post('/api/mod/product', formData, {headers: {
         'Content-Type': 'multipart/form-data'
       }}).then((response) => {
+      })
+    },
+    test2 () {
+      let formData = new FormData()
+      for (var i = 0; i < this.files.length; i++) {
+        let file = this.files[i]
+        formData.append('files[' + i + ']', file)
+      }
+      this.$http.put('/api/mod/product/11/u/img', formData, {headers: {
+        'Content-Type': 'multipart/form-data'
+      }}).then((response) => {
+      })
+    },
+    test3 () {
+      this.$http.delete('/api/mod/product/11').then((response) => {
       })
     }
   }
