c665129f0f9ae9cc936868b0ca2bd19507e6ebe6
yujin100b <jjem22@naver.com>
2018-09-07 17:22:29 +0900

    조금하고 집에 갑니다유

--- front-end/src/components/Projectlist.vue
+++ front-end/src/components/Projectlist.vue
@@ -1,15 +1,41 @@
 <template>
+  <div>
+
+  <div class="controls container">
+      <button type="button" class="control" data-filter="all">All</button>
+      <button type="button" class="control" data-filter=".tour">#투어</button>
+      <button type="button" class="control" data-filter=".design">#디자인</button>
+      <button type="button" class="control" data-filter=".map">#지도</button>
+      <button type="button" class="control" data-filter=".edu">#교육</button>
+      <button type="button" class="control" data-filter=".pre">#강의</button>
+      <button type="button" class="control" data-filter=".local">#지역</button>
+      <button type="button" class="control" data-filter=".city">#도시재생</button>
+      <button type="button" class="control" data-filter=".fest">#축제</button>
+      <button type="button" class="control" data-filter=".evt">#행사</button>
+  </div>
   <div class="container">
     <div class="profile-items">
       <div class="columns is-multiline is-mobile">
 
-        <template  v-for="(n, index) in 10">
-          <div v-bind:key="index" class="column is-one-third">
+        <template  v-for="(n, index) in 3">
+          <div v-bind:key="index" class="column is-one-third mix tour">
             <div class="image is-1by1" @click="openModal(item)">
               <img src="https://via.placeholder.com/256x256"/>
             </div>
           </div>
         </template>
+
+        <template  v-for="(n, index) in 3">
+          <div v-bind:key="index" class="column is-one-third mix">
+            <div class="image is-1by1" @click="openModal(item)">
+              <img src="https://via.placeholder.com/256x256"/>
+            </div>
+          </div>
+        </template>
+
+        <div class="gap"></div>
+        <div class="gap"></div>
+        <div class="gap"></div>
       </div>
       <div class="modal" :class="modalClass">
         <div class="modal-background" @click="closeModal()"></div>
@@ -23,13 +49,17 @@
               </div>
               <div class="column">
                 <h1 class="title">
-                  Some {{ modalData.type }}
+                  Project title {{ modalData.type }}
                 </h1>
                 <h2 class="subtitle">
                   <small>
-                    Curator: {{ modalData.owner.name }}
+                    기간: {{ modalData.owner.name }}
                     <br />
-                    ImageId: {{ modalData.itemId }}
+                    지역: {{ modalData.itemId }}
+                    <br />
+                    협력기관: {{ modalData.itemId }}
+                    <br />
+                    내용및 관련 url: {{ modalData.itemId }}
                   </small>
                 </h2>
                 <p>
@@ -45,14 +75,14 @@
       </div>
     </div>
   </div>
+</div>
 </template>
 
 <script>
 /* eslint-disable */
-import { mapGetters, mapActions } from 'vuex';
+import mixitup from 'mixitup';
 export default {
   name: 'profileItems',
-  props: ['person'],
   data() {
     return {
       isModalVisible: false,
@@ -60,12 +90,6 @@ export default {
     };
   },
   computed: {
-    ...mapGetters([
-      'routeParams',
-      'routeQueryParams',
-      'items',
-      'itemOwner',
-    ]),
     modalClass() {
       return {
         'is-active': this.isModalVisible,
@@ -94,9 +118,10 @@ export default {
     }
   },
   created() {
-    this.fetchProfile('me');
-    this.fetchProfile(this.routeParams.profileId);
-    this.fetchItems();
+  },
+  mounted() {
+    var containerEl = document.querySelector('.container');
+    var mixer = mixitup(containerEl);
   }
 };
 </script>
@@ -126,5 +151,122 @@ export default {
   right: 0;
   top: 4.5em;
   width: 30%;
+}
+
+.controls {
+    margin: auto;
+    padding: 1rem;
+    background: #333;
+    font-size: 2rem;
+}
+
+.control {
+    position: relative;
+    display: inline-block;
+    width: 4rem;
+    height: 2.7rem;
+    background: #444;
+    cursor: pointer;
+    font-size: 10px;
+    color: white;
+    transition: background 150ms;
+    text-decoration: none;
+    text-align: center;
+  }
+
+.control:hover {
+    background: #3f3f3f;
+}
+/*.control[data-filter]:after {
+    content: '';
+    position: absolute;
+    width: 10px;
+    height: 10px;
+    top: calc(50% - 6px);
+    left: calc(50% - 6px);
+    border: 2px solid currentColor;
+    border-radius: 2px;
+    background: currentColor;
+    transition: background-color 150ms, border-color 150ms;
+}*/
+.mixitup-control-active {
+    background: #393939;
+}
+
+.mixitup-control-active[data-filter]:after {
+    background: transparent;
+}
+
+.control:first-of-type {
+    border-radius: 3px 0 0 3px;
+}
+
+.control:last-of-type {
+    border-radius: 0 3px 3px 0;
+}
+
+.control[data-filter] + .control[data-sort] {
+    margin-left: .75rem;
+}
+
+
+/* Container
+---------------------------------------------------------------------- */
+/*
+.container {
+    padding: 1rem;
+    text-align: justify;
+    font-size: 0.1px;
+}
+
+.container:after {
+    content: '';
+    display: inline-block;
+    width: 100%;
+}*/
+
+/* Target Elements
+---------------------------------------------------------------------- */
+.profile-items{
+  padding-top : 1em;
+}
+
+.gap {
+    display: inline-block;
+    vertical-align: top;
+}
+/*
+.mix {
+    background: #fff;
+    border-top: .5rem solid currentColor;
+    border-radius: 2px;
+    margin-bottom: 1rem;
+    position: relative;
+}
+
+.mix:before {
+    content: '';
+    display: inline-block;
+    padding-top: 56.25%;
+}
+*/
+.mix.green {
+    color: #91e6c7;
+}
+
+.mix.pink {
+    color: #d595aa;
+}
+
+.mix.blue {
+    color: #5ecdde;
+}
+/* 3 Columns */
+
+@media screen and (min-width: 541px) {
+    .mix,
+    .gap {
+        width: calc(100%/3 - (((3 - 1) * 1rem) / 3));
+    }
 }
 </style>
